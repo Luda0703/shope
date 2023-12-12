7a7396c26639c3fe3e5b7464b0fa07c7e76b1107
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-12-12 18:38:05 +0200

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import AppRoutes from "../Routes/Routes";
+import Sidebar from "../Sidebar/Sidebar";
+
+const App = () => {
+    return ( 
+        <div className="app">
+            <Header/>
+            
+            <div className="container">
+                <Sidebar/>
+                <AppRoutes/>
+            </div>
+            
+            <Footer/>
+        </div>
+     );
+}
+ 
+export default App;
