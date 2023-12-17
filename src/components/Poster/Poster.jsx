1aad6e95e75bf42af5ac4802c613015cfcc6d94e
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-12-17 15:33:41 +0200

    Products

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -12,7 +12,7 @@ const Poster = () => {
         <div className={styles.text}>
           <div className={styles.subtitle}> the bestseller of 2022 </div>
           <h1 className={styles.head}>LENNON r2d2 with NVIDIA 5090 TI</h1>
-          <button className={styles.button}>shop now</button>
+          <button className={styles.button}>Shop Now</button>
         </div>
         <div className={styles.image}>
           <img src={BG} alt="" />
